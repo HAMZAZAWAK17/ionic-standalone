c1c3a20d65f05151aab035909b0b0ba3697656b6
Ez-zouek Hamza <ezzouekhamza2411@gmail.com>
2026-03-03 15:20:54 +0000

    feat: Implement initial home page with navigation to a calculs page and configure application routes.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
-import { RouterLink } from '@angular/router';
+import { Router} from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, RouterLink],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton],
 })
 export class HomePage {
-  constructor() {}
+  constructor(private router: Router) {}
+
+  goToCalculs() {
+    this.router.navigate(['/calculs']);
+  }
 }
